8fff78834a6568b94c54ccd74a223a66c27b957f
Kulkarni,Bhavika <bhavikakulkarni1@gmail.com>
2022-11-13 19:52:26 +0530

    Committed 1

--- runner.bat
+++ runner.bat
@@ -1,4 +1,2 @@
-cd C:\Users\Prateek\PycharmProjects\Sprint2
+cd C:\Users\Prateek\.jenkins\workspace\Sprint-Flip
 behave -f allure_behave.formatter:AllureFormatter -o ./allure_reports features/ClothProduct.feature
-behave --junit --junit-directory junitreports features/ClothProduct.feature
-behave -f json.pretty -o jreports features/ClothProduct.feature
